238d0eb010e2412b69c72cc2eee10df4c98abd4c
frenddd <flaviusm26@gmail.com>
2023-03-31 23:18:58 +0300

    player movement fixed

--- Cerb/Assets/Scripts/playercontroller.cs
+++ Cerb/Assets/Scripts/playercontroller.cs
@@ -6,6 +6,10 @@ public class playercontroller : MonoBehaviour
 {
     public CharacterController2D controller;
 
+    public float Speed = 40f;
+
+    float horizontalMovement = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +19,11 @@ public class playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Input.GetAxisRaw("Horizontal"));
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * Speed;
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMovement * Time.fixedDeltaTime, false, false);
     }
 }
